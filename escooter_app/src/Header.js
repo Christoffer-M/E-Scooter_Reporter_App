976c792f0a0cb4c5e6edae09abed982d49bd64ee
Albert Bethlowsky Rovsing <arov@itu.dk>
2020-10-02 13:17:00 +0200

    fixed the default

---
+++ escooter_app/src/Header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Header = () => {
+    return (
+        <div>
+        <p> ITS THE END OF THE WORLD AS WE KNOW IT</p>
+        </div>
+    )
+}
+
+export default Header
