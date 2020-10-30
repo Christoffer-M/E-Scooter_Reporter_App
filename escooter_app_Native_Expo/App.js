128b979dbdadc75ce1363f87ef735bcf2b8ca011
Albert Bethlowsky Rovsing <arov@itu.dk>
2020-10-30 10:56:38 +0100

    minor fixes - remember to install everything

--- escooter_app_Native_Expo/App.js
+++ escooter_app_Native_Expo/App.js
@@ -1,17 +1,19 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View} from 'react-native';
-import Buttons from './components/Buttons';
-import Headline from './components/Headline';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import Buttons from "./components/Buttons";
+import Headline from "./components/Headline";
 
 export default function App() {
   return (
-    <View style={styles.container}> 
-      <View style={{marginRight: 15, marginLeft: 15}}>
-            <Headline/>
-            <Text style={styles.fontStyle}>Would you like to sign up or continue as a guest?</Text>
-            <Buttons/>
-            <StatusBar style="auto" />
+    <View style={styles.container}>
+      <View style={{ marginRight: 15, marginLeft: 15 }}>
+        <Headline />
+        <Text style={styles.fontStyle}>
+          Would you like to sign up or continue as a guest?
+        </Text>
+        <Buttons />
+        <StatusBar style="auto" />
       </View>
     </View>
   );
@@ -20,20 +22,20 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#2F4357',
-    alignItems: 'center',
-    justifyContent: 'center', 
+    backgroundColor: "#2F4357",
+    alignItems: "center",
+    justifyContent: "center",
   },
 
   textContainer: {
-    color: '#C0C0C0',
-    width:'500px',
-  }, 
-  
+    color: "#C0C0C0",
+    width: "500px",
+  },
+
   fontStyle: {
-    color: '#FBEFE8',
+    color: "#FBEFE8",
     fontSize: 18,
     lineHeight: 36,
-    textAlign: 'center',
+    textAlign: "center",
   },
 });
