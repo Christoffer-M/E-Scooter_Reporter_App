29cf24893d05aee6f4c4568a5ca77114a1a54159
Christoffer Ry Mouritzen <cmou@itu.dk>
2020-10-30 10:25:08 +0100

    first commit

--- escooter_app_Native_Expo/Screens/CameraScreen.js
+++ escooter_app_Native_Expo/Screens/CameraScreen.js
@@ -10,7 +10,7 @@ import { Camera } from "expo-camera";
 import SvgUri from "expo-svg-uri";
 
 const CameraSceen = ({ navigation }) => {
-  const [hasPermission, setHasPermission] = useState(null);
+    const [hasPermission, setHasPermission] = useState(null);
 
   useEffect(() => {
     (async () => {
