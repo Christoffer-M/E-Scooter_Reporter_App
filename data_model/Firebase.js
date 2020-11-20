94da211b39428f792979cc05166206f9f725fffd
Gabriel Brodersen <gbro@itu.dk>
2020-11-20 14:14:08 +0100

    Added getAddress(maxLength)
    Use getAddress(maxLength) to get the address string capped at a string length of maxLength. If the address is capped, "..." is added at the end

--- data_model/Firebase.js
+++ data_model/Firebase.js
@@ -211,6 +211,16 @@ class Report {
 		return "Unknown location";
 	}
 
+	getAddress(maxLength) {
+		const address = getAddress();
+		if (address.length > maxLength) { // Only cut address if it is too long
+			const length = Math.max(maxLength, 3); // Avoid sub 3 length values
+			return (trimmedString = getAddress().substring(0, length - 3) + "...");
+		} else {
+			return address;
+		}
+	}
+
 	setQR(qrCode) {
 		this.qr = qrCode;
 		this.setBrand(qrCode);
