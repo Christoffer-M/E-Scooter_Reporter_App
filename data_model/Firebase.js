6605435e90597afca9953a9d573cb49f477493db
Christoffer Ry Mouritzen <cmou@itu.dk>
2020-12-18 17:25:15 +0100

    commit

--- data_model/Firebase.js
+++ data_model/Firebase.js
@@ -45,74 +45,6 @@ export const getScooterImage = (scooterImageName) => {
     });
 };
 
-//GOOGLE SIGN IN
-//TODO: FIND A WAY TO UPLOAD AND AUTHENTICATE THE USER WITH FIREBASE. fire.base.auth().currentUser; ??
-export async function signInWithGoogleAsync() {
-  try {
-    const result = await Google.logInAsync({
-      androidClientId:
-        "423566385353-44d6uehk11b0u68gocjk0cad9q2ke0mv.apps.googleusercontent.com",
-      iosClientId:
-        "423566385353-kbvdqmr2s6sca20cca1q30sq8ctk7s35.apps.googleusercontent.com",
-      scopes: ["profile", "email"],
-    });
-
-    if (result.type === "success") {
-      // Build Firebase credential with the Google ID token.
-      var credentials = firebase.auth.GoogleAuthProvider.credential(
-        result.idToken
-      );
-
-      // Sign in with credential from the Google user.
-      const user = firebase
-        .auth()
-        .signInWithCredential(credentials)
-        .catch(function (error) {
-          // Handle Errors here.
-          var errorCode = error.code;
-          var errorMessage = error.message;
-          // The email of the user's account used.
-          var email = error.email;
-          // The firebase.auth.AuthCredential type that was used.
-          var credential = error.credential;
-          // ...
-        });
-      const res = {
-        user: user,
-        type: result.type,
-      };
-      return res;
-    } else {
-      console.log("fail!!!!");
-      alert("Google authentication failed");
-      return { cancelled: true };
-    }
-  } catch (e) {
-    console.log("ERROR!!!!");
-    alert("Sorry. An error has occurred while trying to log in");
-    return { error: true };
-  }
-}
-
-export async function getUser() {
-  const user = firebase.auth();
-  if (user) {
-    console.log("User from firebase: " + user.email);
-  }
-  return user;
-}
-
-export function logout() {
-  firebase
-    .auth()
-    .signOut()
-    .then(function () {
-      console.log("Signed out");
-    })
-    .catch(function (error) {
-      console.log(error);
-    });
-}
 export const getUserReports = (user) => {
   let reportsByID = [];
   db.collection("reports")
@@ -134,181 +66,185 @@ export const getUserReports = (user) => {
 
 //Download report by
 export const getReportByUUID = async (uuid) => {
-	const report = db.collection("reports").doc(uuid);
-	const reportData = null;
+  const report = db.collection("reports").doc(uuid);
+  const reportData = null;
 
-	// Valid options for source are 'server', 'cache', or
-	// 'default'. See https://firebase.google.com/docs/reference/js/firebase.firestore.GetOptions
-	// for more information.
-	var getOptions = {
-		source: "default",
-	};
+  // Valid options for source are 'server', 'cache', or
+  // 'default'. See https://firebase.google.com/docs/reference/js/firebase.firestore.GetOptions
+  // for more information.
+  var getOptions = {
+    source: "default",
+  };
 
-	// Get a report
-	report
-		.get(getOptions)
-		.then(function (reportDoc) {
-			// Document was found in the cache. If no cached document exists,
-			// an error will be returned to the 'catch' block below.
-			//console.log("Cached document data:", report.data());
-			reportData = reportDoc.data();
-		})
-		.catch(function (error) {
-			console.log("Error getting report:", error);
-		});
+  // Get a report
+  report
+    .get(getOptions)
+    .then(function (reportDoc) {
+      // Document was found in the cache. If no cached document exists,
+      // an error will be returned to the 'catch' block below.
+      //console.log("Cached document data:", report.data());
+      reportData = reportDoc.data();
+    })
+    .catch(function (error) {
+      console.log("Error getting report:", error);
+    });
 
-	// Return report
-	return reportData;
+  // Return report
+  return reportData;
 };
 
 //Upload report document
 export const uploadReport = async (report) => {
-	db.collection("reports")
-		.doc(report.uuid)
-		.set({
-			uuid: report.uuid,
-			user: report.user,
-			image: report.imageFileName,
-			timestamp: report.timestamp,
-			geolocation: report.geolocation,
-			address: report.address,
-			qr: report.qr,
-			brand: report.brand,
-			laying: report.laying,
-			broken: report.broken,
-			misplaced: report.misplaced,
-			other: report.other,
-			comment: report.comment,
-		})
-		.then(function (docRef) {
-			console.log("Document written with ID: ", docRef.id);
-		})
-		.catch(function (error) {
-			console.error("Error adding report: ", error);
-			return false;
-		});
-	return true;
+  db.collection("reports")
+    .doc(report.uuid)
+    .set({
+      uuid: report.uuid,
+      user: report.user,
+      image: report.imageFileName,
+      timestamp: report.timestamp,
+      geolocation: report.geolocation,
+      address: report.address,
+      qr: report.qr,
+      brand: report.brand,
+      laying: report.laying,
+      broken: report.broken,
+      misplaced: report.misplaced,
+      other: report.other,
+      comment: report.comment,
+    })
+    .then(function (docRef) {
+      console.log("Document written with ID: ", docRef.id);
+    })
+    .catch(function (error) {
+      console.error("Error adding report: ", error);
+      return false;
+    });
+  return true;
 };
 
 //Delete report document
 export const deleteReport = async (uuid) => {
-	db.collection("reports")
-		.doc(uuid)
-		.delete()
-		.then(function () {
-			console.log("Document successfully deleted with uuid:", uuid);
-		})
-		.catch(function (error) {
-			console.error("Error removing document: ", error);
-			return false;
-		});
-	return true;
+  db.collection("reports")
+    .doc(uuid)
+    .delete()
+    .then(function () {
+      console.log("Document successfully deleted with uuid:", uuid);
+    })
+    .catch(function (error) {
+      console.error("Error removing document: ", error);
+      return false;
+    });
+  return true;
 };
 
 //Upload report photo to Firebase Storage
 export const uploadReportPhoto = async (report) => {
-	const response = await fetch(report.imageURI);
-	const blob = await response.blob();
+  const response = await fetch(report.imageURI);
+  const blob = await response.blob();
 
-	// File or Blob
-	var file = blob;
+  // File or Blob
+  var file = blob;
 
-	// Create the file metadata
-	var metadata = {
-		contentType: "image/jpeg",
-	};
+  // Create the file metadata
+  var metadata = {
+    contentType: "image/jpeg",
+  };
 
-	// Upload photo and metadata to the object 'scooter_photos/[uuid].jpg'
-	var uploadTask = scooterImages.put(file, metadata);
+  // Upload photo and metadata to the object 'scooter_photos/[uuid].jpg'
+  var uploadTask = scooterImages.put(file, metadata);
 
-	// Listen for state changes, errors, and completion of the upload.
-	uploadTask.on(
-		firebase.storage.TaskEvent.STATE_CHANGED, // or 'state_changed'
-		function (snapshot) {
-			// Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
-			var progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
-			console.log(report.imageName, "upload is " + progress + "% done");
-			report.progress = progress / 100.0;
-			switch (snapshot.state) {
-				case firebase.storage.TaskState.PAUSED: // or 'paused'
-					console.log("Upload is paused");
-					break;
-				case firebase.storage.TaskState.RUNNING: // or 'running'
-					console.log("Upload is running");
-					break;
-			}
-		},
-		function (error) {
-			// A full list of error codes is available at
-			// https://firebase.google.com/docs/storage/web/handle-errors
-			switch (error.code) {
-				case "storage/unauthorized":
-					console.log("Upload Error: Firebase Storage not authorized");
-					// User doesn't have permission to access the object
-					console.log(
-						"Upload Error: User",
-						report.user,
-						"does not have permission!"
-					);
-					break;
+  // Listen for state changes, errors, and completion of the upload.
+  uploadTask.on(
+    firebase.storage.TaskEvent.STATE_CHANGED, // or 'state_changed'
+    function (snapshot) {
+      // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
+      var progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+      console.log(report.imageName, "upload is " + progress + "% done");
+      report.progress = progress / 100.0;
+      switch (snapshot.state) {
+        case firebase.storage.TaskState.PAUSED: // or 'paused'
+          console.log("Upload is paused");
+          break;
+        case firebase.storage.TaskState.RUNNING: // or 'running'
+          console.log("Upload is running");
+          break;
+      }
+    },
+    function (error) {
+      // A full list of error codes is available at
+      // https://firebase.google.com/docs/storage/web/handle-errors
+      switch (error.code) {
+        case "storage/unauthorized":
+          console.log("Upload Error: Firebase Storage not authorized");
+          // User doesn't have permission to access the object
+          console.log(
+            "Upload Error: User",
+            report.user,
+            "does not have permission!"
+          );
+          break;
 
-				case "storage/unknown":
-					// Unknown error occurred, inspect error.serverResponse
-					console.log("Upload Error: Unknown error occurred!");
-					break;
-			}
-			return false;
-		},
-		function () {
-			// Upload completed successfully, now we can get the download URL
-			uploadTask.snapshot.ref.getDownloadURL().then(function (downloadURL) {
-				console.log("File available at", downloadURL);
-			});
-		}
-	);
-	report.progress = 1.0;
-	return true;
+        case "storage/unknown":
+          // Unknown error occurred, inspect error.serverResponse
+          console.log("Upload Error: Unknown error occurred!");
+          break;
+      }
+      return false;
+    },
+    function () {
+      // Upload completed successfully, now we can get the download URL
+      uploadTask.snapshot.ref.getDownloadURL().then(function (downloadURL) {
+        console.log("File available at", downloadURL);
+      });
+    }
+  );
+  report.progress = 1.0;
+  return true;
 };
 
 const getDownloadReportPhotoURL = async (report) => {
-	// Create a reference to the file we want to download
-	let imagePath = scooterFolderPath + "/" + report.imageName
-	let starsRef = storageRef.child(imagePath);
-	let downloadURL = ""
+  // Create a reference to the file we want to download
+  let imagePath = scooterFolderPath + "/" + report.imageName;
+  let starsRef = storageRef.child(imagePath);
+  let downloadURL = "";
 
-	// Get the download URL
-	starsRef
-		.getDownloadURL()
-		.then(function (url) {
-			downloadURL = url
-			// Insert url into an <img> tag to "download"
-		})
-		.catch(function (error) {
-			// A full list of error codes is available at
-			// https://firebase.google.com/docs/storage/web/handle-errors
-			switch (error.code) {
-				case "storage/object-not-found":
-					// File doesn't exist
-					console.log("Download URL error: File",imagePath,"doesn't exist!")
-					break;
+  // Get the download URL
+  starsRef
+    .getDownloadURL()
+    .then(function (url) {
+      downloadURL = url;
+      // Insert url into an <img> tag to "download"
+    })
+    .catch(function (error) {
+      // A full list of error codes is available at
+      // https://firebase.google.com/docs/storage/web/handle-errors
+      switch (error.code) {
+        case "storage/object-not-found":
+          // File doesn't exist
+          console.log("Download URL error: File", imagePath, "doesn't exist!");
+          break;
 
-				case "storage/unauthorized":
-					// User doesn't have permission to access the object
-					console.log("Download URL error: User doesn't have permission to access the object!")
-					break;
+        case "storage/unauthorized":
+          // User doesn't have permission to access the object
+          console.log(
+            "Download URL error: User doesn't have permission to access the object!"
+          );
+          break;
 
-				case "storage/canceled":
-					// User canceled the download
-					console.log("Download URL error: User canceled the download!")
-					break;
+        case "storage/canceled":
+          // User canceled the download
+          console.log("Download URL error: User canceled the download!");
+          break;
 
-				case "storage/unknown":
-					// Unknown error occurred, inspect the server response
-					console.log("Download URL error: Unknown error occurred, inspect the server response")
-					break;
-			}
-			console.log("Image from",imagePath,"was not downloaded")
-			return "";
-		});
-	return downloadURL;
-}
+        case "storage/unknown":
+          // Unknown error occurred, inspect the server response
+          console.log(
+            "Download URL error: Unknown error occurred, inspect the server response"
+          );
+          break;
+      }
+      console.log("Image from", imagePath, "was not downloaded");
+      return "";
+    });
+  return downloadURL;
+};
--- data_model/Storage.js
+++ data_model/Storage.js
@@ -13,105 +13,171 @@ export let reportsNotUploaded = [];
 export let reportImagesNotUploaded = [];
 
 export function isSignedIn() {
-	return this.user.length > 0;
+  return this.user.length > 0;
 }
 
 export function signOut() {
-	this.user = "";
+  this.user = "";
 }
 
 // Get a new report
 // Remember to call this first when starting on a new report!
 export function newReport() {
-	this.report = Report(user);
-	return this.report;
+  this.report = Report(user);
+  return this.report;
 }
 
-// Submit by uploading the (new) report to Firebase and add it to local storage 
+// Submit by uploading the (new) report to Firebase and add it to local storage
 //TODO NEED UPLOAD PHOTO TO FIREBASE
 export function submitReport() {
-	if (this.report.isSubmittable()) {
-		if (this.addReport(this.report)) {            
-            if (Firebase.uploadReport (report)) {
-                console.log("Report uploaded successfully to FireBase with uuid:", report.uuid)
-                return true;
-            } else {
-                console.log("Failed to upload report to FireBase with uuid:", report.uuid)
-                // Saving report for later submission when internet has recoved (if not already saved)
-                for (report in this.reportsNotUploaded) if (this.report.uuid == report.uuid) return false;
-                console.log("Saving report for upload later...")
-                this.reportsNotUploaded.push(report);
-                return false;
-            }
-        } else {
-            throw "Error: Report submission cancelled, as it already exists in Storage (somehow)!";
-        }
-	} else {
-		throw "Error: Report missing information! check .isSubmittable() first before trying to submit!";
-	}
+  if (this.report.isSubmittable()) {
+    if (this.addReport(this.report)) {
+      if (Firebase.uploadReport(report)) {
+        console.log(
+          "Report uploaded successfully to FireBase with uuid:",
+          report.uuid
+        );
+        return true;
+      } else {
+        console.log(
+          "Failed to upload report to FireBase with uuid:",
+          report.uuid
+        );
+        // Saving report for later submission when internet has recoved (if not already saved)
+        for (report in this.reportsNotUploaded)
+          if (this.report.uuid == report.uuid) return false;
+        console.log("Saving report for upload later...");
+        this.reportsNotUploaded.push(report);
+        return false;
+      }
+    } else {
+      throw "Error: Report submission cancelled, as it already exists in Storage (somehow)!";
+    }
+  } else {
+    throw "Error: Report missing information! check .isSubmittable() first before trying to submit!";
+  }
 }
 
 // Delete reports from Firebase and Storage
 export function deleteReport(uuid) {
-    if (Firebase.deleteReport(uuid)) {
-        if (this.removeReport(report)) {
-            console.log("Report deleted from both Firebase and Storage")
-        } else {
-            throw "Error: Report deleted from Firebase, but was never found in Storage!!"
-        }
-        return true;
+  if (Firebase.deleteReport(uuid)) {
+    if (this.removeReport(report)) {
+      console.log("Report deleted from both Firebase and Storage");
     } else {
-        console.log("Document was not deleted with uuid:", uuid);
-        return false;
+      throw "Error: Report deleted from Firebase, but was never found in Storage!!";
     }
+    return true;
+  } else {
+    console.log("Document was not deleted with uuid:", uuid);
+    return false;
+  }
 }
 
 //TODO
 export function syncReports(report) {}
 
-
-
-
-
-
-
-
-
-
-
-
-
 //PRIVATE FUNCTIONS
 //Add report from local storage
 function addReport(report) {
-	if (this.reports.has(reports.uuid)) {
-		console.log(
-			"Report could not be added to Storage (already exists) with uuid",
-			report.uuid
-		);
-		return false;
-	} else {
-		this.reports.set(report.uuid, report);
-		console.log("Report added to Storage with uuid", report.uuid);
-		return true;
-	}
+  if (this.reports.has(reports.uuid)) {
+    console.log(
+      "Report could not be added to Storage (already exists) with uuid",
+      report.uuid
+    );
+    return false;
+  } else {
+    this.reports.set(report.uuid, report);
+    console.log("Report added to Storage with uuid", report.uuid);
+    return true;
+  }
 }
 
 //Remove report from local storage
 function removeReport(report) {
-	if (this.reports.delete(report.uuid)) {                
-        // If to-be-deleted report was in pending upload array, also delete it from there:
-        const filtered = this.reportsNotUploaded.filter(r => r.uuid != report.uuid);
-        this.reportsNotUploaded = filtered
-        console.log("Report deleted from Storage with uuid", report.uuid);
-		return true;
-	} else {
-		console.log(
-			"Report could not be found and deleted from Storage with uuid",
-			report.uuid
-		);
-		return false;
-	}
+  if (this.reports.delete(report.uuid)) {
+    // If to-be-deleted report was in pending upload array, also delete it from there:
+    const filtered = this.reportsNotUploaded.filter(
+      (r) => r.uuid != report.uuid
+    );
+    this.reportsNotUploaded = filtered;
+    console.log("Report deleted from Storage with uuid", report.uuid);
+    return true;
+  } else {
+    console.log(
+      "Report could not be found and deleted from Storage with uuid",
+      report.uuid
+    );
+    return false;
+  }
+}
+
+//GOOGLE SIGN IN
+//TODO: FIND A WAY TO UPLOAD AND AUTHENTICATE THE USER WITH FIREBASE. fire.base.auth().currentUser; ??
+export async function signInWithGoogleAsync() {
+  try {
+    const result = await Google.logInAsync({
+      androidClientId:
+        "423566385353-44d6uehk11b0u68gocjk0cad9q2ke0mv.apps.googleusercontent.com",
+      iosClientId:
+        "423566385353-kbvdqmr2s6sca20cca1q30sq8ctk7s35.apps.googleusercontent.com",
+      scopes: ["profile", "email"],
+    });
+
+    if (result.type === "success") {
+      // Build Firebase credential with the Google ID token.
+      var credentials = firebase.auth.GoogleAuthProvider.credential(
+        result.idToken
+      );
+
+      // Sign in with credential from the Google user.
+      const user = firebase
+        .auth()
+        .signInWithCredential(credentials)
+        .catch(function (error) {
+          // Handle Errors here.
+          var errorCode = error.code;
+          var errorMessage = error.message;
+          // The email of the user's account used.
+          var email = error.email;
+          // The firebase.auth.AuthCredential type that was used.
+          var credential = error.credential;
+          // ...
+        });
+      const res = {
+        user: user,
+        type: result.type,
+      };
+      return res;
+    } else {
+      console.log("fail!!!!");
+      alert("Google authentication failed");
+      return { cancelled: true };
+    }
+  } catch (e) {
+    console.log("ERROR!!!!");
+    alert("Sorry. An error has occurred while trying to log in");
+    return { error: true };
+  }
+}
+
+export async function getUser() {
+  const user = firebase.auth();
+  if (user) {
+    console.log("User from firebase: " + user.email);
+  }
+  return user;
+}
+
+export function logout() {
+  firebase
+    .auth()
+    .signOut()
+    .then(function () {
+      console.log("Signed out");
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
 }
 
 //TODO
@@ -119,5 +185,3 @@ function downloadReport() {}
 
 //TODO
 function deleteReportPhoto(report) {}
-
-
