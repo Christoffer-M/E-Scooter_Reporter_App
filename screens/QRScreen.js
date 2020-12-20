ca632b92ea65dc3cb30d2d1c90c7ec987bdefc44
Julie Schelhart <julieschelhart@Julies-MacBook-Air.local>
2020-12-20 14:46:06 +0100

    deleted alert box to navigate directly to category page

--- screens/QRScreen.js
+++ screens/QRScreen.js
@@ -22,24 +22,25 @@ const QRScreen = ({ navigation }) => {
   const handleBarCodeScanned = ({ type, data }) => {
     storage.getReport().setQR(data);
     setScanned(true);
-    Alert.alert(
-      "Bar code Scanned!",
-      "Bar code with type " + type + " and data " + data + " has been scanned!",
-      [
-        {
-          text: "Scan again",
-          onPress: () => setScanned(false),
-          style: "cancel",
-        },
-        {
-          text: "Continue",
-          onPress: () => {
-            navigation.push("Category");
-          },
-        },
-      ],
-      { cancelable: false }
-    );
+    navigation.push("Category");
+    //   Alert.alert(
+    //     "Bar code Scanned!",
+    //     "Bar code with type " + type + " and data " + data + " has been scanned!",
+    //     [
+    //       {
+    //         text: "Scan again",
+    //         onPress: () => setScanned(false),
+    //         style: "cancel",
+    //       },
+    //       {
+    //         text: "Continue",
+    //         onPress: () => {
+    //           navigation.push("Category");
+    //         },
+    //       },
+    //     ],
+    //     { cancelable: false }
+    //   );
   };
 
   if (hasPermission === null) {
